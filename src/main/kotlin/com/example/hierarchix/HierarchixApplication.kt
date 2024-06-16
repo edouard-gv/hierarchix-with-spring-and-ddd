4c0cee6578f73c9c4912814a01495190e06999f3
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2024-06-16 15:58:13 +0200

    Default IntelliJ Spring scaffolding

---
+++ src/main/kotlin/com/example/hierarchix/HierarchixApplication.kt
@@ -0,0 +1,11 @@
+package com.example.hierarchix
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HierarchixApplication
+
+fun main(args: Array<String>) {
+    runApplication<HierarchixApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/hierarchix/HierarchixApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.hierarchix
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HierarchixApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
